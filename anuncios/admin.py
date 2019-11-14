9f9207045c454d260504928d9955704e8dfd82bf
Guilherme Junqueira <contato@solyd.com.br>
2019-11-14 20:23:50 -0300

    criado o model anuncio

--- anuncios/admin.py
+++ anuncios/admin.py
@@ -3,3 +3,4 @@ from django.contrib import admin
 from anuncios import models
 
 admin.site.register(models.Categoria)
+admin.site.register(models.Anuncio)
--- anuncios/models.py
+++ anuncios/models.py
@@ -6,3 +6,15 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.titulo
+
+
+class Anuncio(models.Model):
+    titulo = models.CharField(max_length=40)
+    descricao = models.TextField(null=True, blank=True)
+    preco = models.DecimalField(max_digits=11, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    alterado_em = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.titulo
